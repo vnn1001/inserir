e6c661c49f3da9ce72d4aa8eda5976d1842ef991
vnn1001 <vinicius.conceicao18@etec.sp.gov.br>
2019-11-11 20:16:55 -0300

    inserir

---
+++ inserir.sql
@@ -0,0 +1,7 @@
+use jogos;
+
+insert into tipolocal (descricao_tipolocal) values ('Residencial');
+insert into tipolocal (descricao_tipolocal) values ('Comercial');
+insert into tipolocal (descricao_tipolocal) values ('Pessoal');
+select * from tipolocal;
+
